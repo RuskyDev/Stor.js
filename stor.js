687a9d2aaa4da7e7857a74c2834ea8cda450393b
RuskyDev <iamayaanalee@gmail.com>
2023-05-09 15:46:22 +0500

    stor.js

--- stor.js
+++ stor.js
@@ -1,57 +1,54 @@
-var Stor = {};
-
-Stor.isSupported = function() {
-  try {
-    var testKey = '__test__';
-    localStorage.setItem(testKey, testKey);
-    localStorage.removeItem(testKey);
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
-
-Stor.set = function(key, value, options) {
-  if (!Stor.isSupported()) {
-    return false;
-  }
-  options = options || {};
-  var expires = options.expires;
-  if (typeof expires === 'number') {
-    expires = new Date(new Date().getTime() + expires * 1000);
-  }
-  var data = {
-    value: value,
-    expires: expires ? expires.getTime() : null
+var Stor = (function () {
+  var isSupported = function () {
+    try {
+      var storage = window.localStorage;
+      var testKey = "__storjs_test__";
+      storage.setItem(testKey, testKey);
+      storage.removeItem(testKey);
+      return true;
+    } catch (e) {
+      return false;
+    }
   };
-  localStorage.setItem(key, JSON.stringify(data));
-  return true;
-};
 
-Stor.get = function(key, defaultValue) {
-  if (!Stor.isSupported()) {
-    return defaultValue;
-  }
-  var value = localStorage.getItem(key);
-  if (!value) {
-    return defaultValue;
-  }
-  try {
-    var data = JSON.parse(value);
-    if (data.expires && new Date().getTime() > data.expires) {
-      localStorage.removeItem(key);
+  var set = function (key, value, options) {
+    var storage = window.localStorage;
+    var data = { value: value };
+    if (options && options.expires) {
+      var date = new Date();
+      date.setTime(date.getTime() + options.expires * 1000);
+      data.expires = date.getTime();
+    }
+    storage.setItem(key, JSON.stringify(data));
+  };
+
+  var get = function (key, defaultValue) {
+    var storage = window.localStorage;
+    var data = storage.getItem(key);
+    if (!data) {
+      return defaultValue;
+    }
+    try {
+      data = JSON.parse(data);
+    } catch (e) {
+      return defaultValue;
+    }
+    if (data.expires && data.expires < new Date().getTime()) {
+      storage.removeItem(key);
       return defaultValue;
     }
     return data.value;
-  } catch (e) {
-    return defaultValue;
-  }
-};
+  };
 
-Stor.remove = function(key) {
-  if (!Stor.isSupported()) {
-    return false;
-  }
-  localStorage.removeItem(key);
-  return true;
-};
+  var remove = function (key) {
+    var storage = window.localStorage;
+    storage.removeItem(key);
+  };
+
+  return {
+    isSupported: isSupported,
+    set: set,
+    get: get,
+    remove: remove,
+  };
+})();
